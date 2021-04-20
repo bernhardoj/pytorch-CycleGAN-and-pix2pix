f78446ef9293da46b5822f65b7f94d27140df6ae
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2021-04-20 14:29:26 +0800

    test please

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     dataset = data_loader.load_data()
     dataset_size = len(data_loader)
     print('#training images = %d' % dataset_size)
+    
 
     model = create_model(opt)
     model.setup(opt)
